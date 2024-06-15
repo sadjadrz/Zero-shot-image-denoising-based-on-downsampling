723bbd44e80b1602f0a3fdcd61bf044c2c486020
fatemeh soleymani <56516238+fatislm@users.noreply.github.com>
2024-06-16 01:44:51 +0330

    Update loss.py

--- metrics/loss.py
+++ metrics/loss.py
@@ -2,237 +2,134 @@ import torch
 import  pywt
 import torch.nn.functional as F
 from Utils.img_downsampler import *
+
 def mse(gt: torch.Tensor, pred: torch.Tensor) -> torch.Tensor:
     loss = torch.nn.MSELoss()
     return loss(gt, pred)
-    
-def gaussian_kernel(size, sigma):
-    x = torch.arange(-size // 2 + 1., size // 2 + 1.)
-    x = x.repeat(size, 1)
-    y = x.t()
-    kernel = torch.exp(-0.5 * (x**2 + y**2) / sigma**2)
-    kernel = kernel / kernel.sum()
-    return kernel.unsqueeze(0).unsqueeze(0)
 
-class GaussionSmoothLayer(nn.Module):
-    def __init__(self, channel, kernel_size, sigma, dim=2):
-        super(GaussionSmoothLayer, self).__init__()
-        kernel_x = cv2.getGaussianKernel(kernel_size, sigma)
-        kernel_y = cv2.getGaussianKernel(kernel_size, sigma)
-        kernel = kernel_x * kernel_y.T
-        self.kernel_data = kernel
-        self.groups = channel
-        if dim == 1:
-            self.conv = nn.Conv1d(in_channels=channel, out_channels=channel, kernel_size=kernel_size,
-                                  groups=channel, bias=False)
-        elif dim == 2:
-            self.conv = nn.Conv2d(in_channels=channel, out_channels=channel, kernel_size=kernel_size,
-                                  groups=channel, bias=False)
-        elif dim == 3:
-            self.conv = nn.Conv3d(in_channels=channel, out_channels=channel, kernel_size=kernel_size,
-                                  groups=channel, bias=False)
-            raise RuntimeError('input dim is not supported !, please check it !')
+def high_pass_filter(img, cutoff):
+    fft_img = torch.fft.fftshift(torch.fft.fftn(img, dim=(-2, -1), norm='ortho'))
+    h, w = fft_img.shape[-2:]
+    mask = torch.zeros_like(fft_img)
+    mask[:, :, h//2-cutoff:h//2+cutoff, w//2-cutoff:w//2+cutoff] = 1
+    filtered_fft = fft_img * (1 - mask)
+    return torch.fft.ifftn(torch.fft.ifftshift(filtered_fft), dim=(-2, -1), norm='ortho').real
 
-        self.conv.weight.requires_grad = False
-        for name, f in self.named_parameters():
-            f.data.copy_(torch.from_numpy(kernel))
-        self.pad = int((kernel_size - 1) / 2)
+def low_pass_filter(img, cutoff):
+    fft_img = torch.fft.fftshift(torch.fft.fftn(img, dim=(-2, -1), norm='ortho'))
+    h, w = fft_img.shape[-2:]
+    mask = torch.zeros_like(fft_img)
+    mask[:, :, h//2-cutoff:h//2+cutoff, w//2-cutoff:w//2+cutoff] = 1
+    filtered_fft = fft_img * mask
+    return torch.fft.ifftn(torch.fft.ifftshift(filtered_fft), dim=(-2, -1), norm='ortho').real
 
-    def forward(self, input):
-        intdata = input
-        intdata = F.pad(intdata, (self.pad, self.pad, self.pad, self.pad), mode='reflect')
-        output = self.conv(intdata)
-        return output
-
-# Initialize necessary components for BGM loss
-BGBlur_kernel = [3, 9, 15]
-BlurWeight = [0.01, 0.1, 1.0]
-BlurNet = [GaussionSmoothLayer(3, k_size, 25).cuda() for k_size in BGBlur_kernel]
-
-def loss_func(noisy_img , model, method_type='modified1'):
-    if method_type == 'modified':
-        noisy1, noisy2 = pair_downsampler1(noisy_img)
-    else:
-        noisy1, noisy2 = pair_downsampler(noisy_img)
-    pred1 = noisy1 - model(noisy1)
-    pred2 = noisy2 - model(noisy2)
-
-    loss_res = 1 / 2 * (mse(noisy1, pred2) + mse(noisy2, pred1))
-
-    noisy_denoised = noisy_img - model(noisy_img)
-    if method_type == 'modified1':
-        denoised1, denoised2 = pair_downsampler1(noisy_denoised)
-    else:
-        denoised1, denoised2 = pair_downsampler(noisy_denoised)
-
-    loss_cons = 1 / 2 * (mse(pred1, denoised1) + mse(pred2, denoised2))
-
-    bgm_loss1 = 0
-    bgm_loss2 = 0
-    bgm_loss = 0
-
-    for index, weight in enumerate(BlurWeight):
-        out_b1 = BlurNet[index](noisy_img)
-        out_real_b1 = BlurNet[index](noisy1)
-        out_b2 = BlurNet[index](noisy_denoised)
-        out_real_b2 = BlurNet[index](noisy2)
-        grad_loss_b1 = mse(out_b1, out_real_b1)
-        grad_loss_b2 = mse(out_b2, out_real_b2)
-        bgm_loss1 += weight * grad_loss_b1
-        bgm_loss2 += weight * grad_loss_b2
-        bgm_loss += bgm_loss1 + bgm_loss2
-        
-    loss = loss_res + loss_cons + bgm_loss
-
-    return loss
-
-def loss_func13(noisy_img , model, method_type):
-    if method_type == 'modified':
-        noisy1, noisy2 = pair_downsampler1(noisy_img)
-        noisy11, noisy22 = pair_downsampler2(noisy_img)
-    else:
-        noisy1, noisy2 = pair_downsampler(noisy_img)
-    pred1 = noisy1 - model(noisy1)
-    pred2 = noisy2 - model(noisy2)
-
-    pred11 = noisy11 - model(noisy11)
-    pred22 = noisy22 - model(noisy22)
-
-    loss_res1 = 1 / 2 * (mse(noisy11, pred22) + mse(noisy22, pred11))
-    loss_res = 1 / 2 * (mse(noisy1, pred2) + mse(noisy2, pred1))
-
-    noisy_denoised = noisy_img - model(noisy_img)
-    if method_type == 'modified':
-        denoised1, denoised2 = pair_downsampler1(noisy_denoised)
-        denoised11, denoised22 = pair_downsampler2(noisy_denoised)
-    else:
-        denoised1, denoised2 = pair_downsampler(noisy_denoised)
-
-    loss_cons1 = 1 / 2 * (mse(pred1, denoised1) + mse(pred2, denoised2))
-    loss_cons = 1 / 2 * (mse(pred11, denoised11) + mse(pred22, denoised22))
-
-    grad_x = torch.abs(denoised1[:, :, :, :-1] - denoised1[:, :, :, 1:])
-    grad_y = torch.abs(denoised1[:, :, :-1, :] - denoised1[:, :, 1:, :])
-    tv_loss = torch.mean(grad_x) + torch.mean(grad_y)
-
-    loss_cons_total = (loss_cons1 + loss_cons)/2
-    loss_res_total = (loss_res + loss_res1)/2
-    #loss = loss_res + loss_cons + 0.004 * tv_loss
-    #loss = 0.8 * (loss_res + loss_cons) + 0.2 * loss_res1
-    loss = loss_cons_total + loss_res_total
-
-    return loss
-
-
-def loss_func1(noisy_img , model, method_type):
-
+def loss_func(noisy_img):
+    # Pair downsampling
     noisy1, noisy2 = pair_downsampler(noisy_img)
-    noisy3, noisy4 = pair_downsampler5(noisy_img)
-    noisy5, noisy6 = pair_downsampler1(noisy_img)
-    pred1 = noisy1 - model(noisy1)
-    pred2 = noisy2 - model(noisy2)
-    pred3 = noisy3 - model(noisy3)
-    pred4 = noisy4 - model(noisy4)
-    pred5 = noisy5 - model(noisy5)
-    pred6 = noisy6 - model(noisy6)
-
-    loss_res = 1 / 6 * (mse(noisy1, pred2) + mse(noisy2, pred1) + mse(noisy3,pred4) + mse(noisy4,pred3) + mse(noisy5,pred6) + mse(noisy6, pred5))
-
-    noisy_denoised = noisy_img - model(noisy_img)
-    if method_type == 'modified':
-        denoised1, denoised2 = pair_downsampler(noisy_denoised)
-        denoised3, denoised4 = pair_downsampler5(noisy_denoised)
-        denoised5, denoised6 = pair_downsampler1(noisy_denoised)
-    else:
-        denoised1, denoised2 = pair_downsampler(noisy_denoised)
-
-    loss_cons = 1 / 6 * (mse(pred1, denoised1) + mse(pred2, denoised2) + (mse(pred3,denoised3)
-    + (mse(pred4, denoised4)) + mse(pred5, denoised5) + mse(pred6, denoised6)))
-
-    #new_loss
-
-    loss_cons1 = 1 / 6 * (mse(noisy1, denoised1) + mse(noisy2, denoised2) + (mse(noisy3,denoised3)
-    + (mse(noisy4, denoised4)) + mse(noisy5, denoised5) + mse(noisy6, denoised6)))
-
-
-    loss = loss_res + loss_cons
-    return loss
-
-def loss_func2(noisy_img , model, method_type):
-
-    noisy1, noisy2 = pair_downsampler1(noisy_img)
-    pred1 = noisy1 - model(noisy1)
-    pred2 = noisy2 - model(noisy2)
-
-    loss_res = 1 / 2 * (mse(noisy1, pred2) + mse(noisy2, pred1))
-
-    noisy_denoised = noisy_img - model(noisy_img)
-    if method_type == 'modified':
-        denoised1, denoised2 = pair_downsampler1(noisy_denoised)
-    else:
-        denoised1, denoised2 = pair_downsampler1(noisy_denoised)
-
-    loss_cons = 1 / 2 * (mse(pred1, denoised1) + mse(pred2, denoised2))
-
-    loss = loss_res + loss_cons
-
-    return loss
-
-def loss_func33(noisy_img , model, method_type, down1=None , down2=None):
-
-    noisy1, noisy2 = pair_downsampler1(noisy_img)
-
-
-    if down1 is not None and down2 is not None:
-        mean_sample = torch.mean(torch.stack([down1, down2]), dim=0)
-        noisy1,noisy2 = pair_downsampler3(mean_sample)
+    noisy11, noisy21 = pair_downsampler1(noisy_img)
+    noisy12, noisy22 = pair_downsampler2(noisy_img)
 
     pred1 = noisy1 - model(noisy1)
     pred2 = noisy2 - model(noisy2)
-
-    loss_res = 1 / 2 * (mse(noisy1, pred2) + mse(noisy2, pred1))
-
-    noisy_denoised = noisy_img - model(noisy_img)
-    if method_type == 'modified':
-        denoised1, denoised2 = pair_downsampler1(noisy_denoised)
-    else:
-        denoised1, denoised2 = pair_downsampler(noisy_denoised)
-
-    loss_cons = 1 / 2 * (mse(pred1, denoised1) + mse(pred2, denoised2))
-
-    loss = loss_res + loss_cons
-
-    return loss
-
-def loss_func3(noisy_img , model, method_type, down1=None , down2=None):
-
-    noisy1, noisy2 = pair_downsampler(down1)
-    noisy11, noisy22 = pair_downsampler(down2)
-
-    pred1 = noisy1 - model(noisy1)
-    pred2 = noisy2 - model(noisy2)
-
     pred11 = noisy11 - model(noisy11)
+    pred21 = noisy21 - model(noisy21)
+    pred12 = noisy12 - model(noisy12)
     pred22 = noisy22 - model(noisy22)
 
-    loss_res = 1 / 2 * (mse(noisy1, pred2) + mse(noisy2, pred1))
-    loss_res1 = 1 / 2 * (mse(noisy11, pred22) + mse(noisy22, pred11))
+    #1
     noisy_denoised = noisy_img - model(noisy_img)
-
     denoised1, denoised2 = pair_downsampler(noisy_denoised)
+    denoised11, denoised21 = pair_downsampler1(noisy_denoised)
+    denoised12, denoised22 = pair_downsampler2(noisy_denoised)
 
-    loss_cons = 1 / 2 * (mse(pred1, denoised1) + mse(pred2, denoised2))
+    # Calculate MSE loss
+    loss_res = (1/6) * sum([
+      (mse(noisy1, pred2) + mse(noisy2, pred1)) +
+      (mse(noisy11, pred21) + mse(noisy21, pred11)) +
+      (mse(noisy12, pred22) + mse(noisy22, pred12))
+       for mse in [MSELoss()] * 6  # Assuming you have 12 MSE terms
+    ])
 
-    loss = loss_res + loss_cons + 0.2*loss_res1
+    loss_cons = (1/6) * (
+        (mse(pred1, denoised1) + mse(pred2, denoised2)) +
+        (mse(pred11, denoised11) + mse(pred21, denoised21)) +
+        (mse(pred12, denoised12) + mse(pred22, denoised22))
+    )
 
-    return loss
+    mse_loss = loss_res + loss_cons
 
-def loss_residual (model , noisy_image1, noisy_image2):
-    pred1 = noisy_image1 - model(noisy_image1)
-    pred2 = noisy_image2 - model(noisy_image2)
-    loss_res = 1 / 2 * (mse(noisy_image1, pred2) + mse(noisy_image2, pred1))
-    return pred1,pred2,loss_res
+    # High-pass MSE loss
+    # High-pass filtered images
+    noisy1_high = high_pass_filter(noisy1, cutoff=30)
+    noisy2_high = high_pass_filter(noisy2, cutoff=30)
+    noisy11_high = high_pass_filter(noisy11, cutoff=30)
+    noisy21_high = high_pass_filter(noisy21, cutoff=30)
+    noisy12_high = high_pass_filter(noisy12, cutoff=30)
+    noisy22_high = high_pass_filter(noisy22, cutoff=30)
 
-def loss_consistency(model, noisy_img,pred1, pred2, denoised1, denoised2):
-    loss_cons = 1 / 2 * (mse(pred1, denoised1) + mse(pred2, denoised2))
-    return loss_cons
+    pred1_high = high_pass_filter(pred1, cutoff=30)
+    pred2_high = high_pass_filter(pred2, cutoff=30)
+    pred11_high = high_pass_filter(pred11, cutoff=30)
+    pred21_high = high_pass_filter(pred21, cutoff=30)
+    pred12_high = high_pass_filter(pred12, cutoff=30)
+    pred22_high = high_pass_filter(pred22, cutoff=30)
+
+    denoised1_high = high_pass_filter(denoised1, cutoff=30)
+    denoised2_high = high_pass_filter(denoised2, cutoff=30)
+    denoised11_high = high_pass_filter(denoised11, cutoff=30)
+    denoised21_high = high_pass_filter(denoised21, cutoff=30)
+    denoised12_high = high_pass_filter(denoised12, cutoff=30)
+    denoised22_high = high_pass_filter(denoised22, cutoff=30)
+
+    high_pass_mse_loss_res = (1/6) * (
+        F.mse_loss(noisy1_high, pred2_high) + F.mse_loss(noisy2_high, pred1_high) +
+        F.mse_loss(noisy11_high, pred21_high) + F.mse_loss(noisy21_high, pred11_high) +
+        F.mse_loss(noisy12_high, pred22_high) + F.mse_loss(noisy22_high, pred12_high)
+    )
+
+    high_pass_mse_loss_cons = (1/6) * (
+        F.mse_loss(pred1_high, denoised1_high) + F.mse_loss(pred2_high, denoised2_high) + 
+        F.mse_loss(pred11_high, denoised11_high) + F.mse_loss(pred21_high, denoised21_high) + 
+        F.mse_loss(pred12_high, denoised12_high) + F.mse_loss(pred22_high, denoised22_high)
+    )
+    high_pass_mse_loss = high_pass_mse_loss_res + high_pass_mse_loss_cons
+
+    # Low-pass MSE loss
+    # Low-pass filtered images
+    noisy1_low = low_pass_filter(noisy1, cutoff=20)
+    noisy2_low = low_pass_filter(noisy2, cutoff=20)
+    noisy11_low = low_pass_filter(noisy11, cutoff=20)
+    noisy21_low = low_pass_filter(noisy21, cutoff=20)
+    noisy12_low = low_pass_filter(noisy12, cutoff=20)
+    noisy22_low = low_pass_filter(noisy22, cutoff=20)
+
+    pred1_low = low_pass_filter(pred1, cutoff=20)
+    pred2_low = low_pass_filter(pred2, cutoff=20)
+    pred11_low = low_pass_filter(pred11, cutoff=20)
+    pred21_low = low_pass_filter(pred21, cutoff=20)
+    pred12_low = low_pass_filter(pred12, cutoff=20)
+    pred22_low = low_pass_filter(pred22, cutoff=20)
+
+    denoised1_low = low_pass_filter(denoised1, cutoff=20)
+    denoised2_low = low_pass_filter(denoised2, cutoff=20)
+    denoised11_low = low_pass_filter(denoised11, cutoff=20)
+    denoised21_low = low_pass_filter(denoised21, cutoff=20)
+    denoised12_low = low_pass_filter(denoised12, cutoff=20)
+    denoised22_low = low_pass_filter(denoised22, cutoff=20)
+
+    low_pass_mse_loss_res = (1/6) * (
+        F.mse_loss(noisy1_low, pred2_low) + F.mse_loss(noisy2_low, pred1_low) +
+        F.mse_loss(noisy11_low, pred21_low) + F.mse_loss(noisy21_low, pred11_low) +
+        F.mse_loss(noisy12_low, pred22_low) + F.mse_loss(noisy22_low, pred12_low)
+    )
+
+    low_pass_mse_loss_cons = (1/6) * (
+        F.mse_loss(pred1_low, denoised1_low) + F.mse_loss(pred2_low, denoised2_low) + 
+        F.mse_loss(pred11_low, denoised11_low) + F.mse_loss(pred21_low, denoised21_low) + 
+        F.mse_loss(pred12_low, denoised12_low) + F.mse_loss(pred22_low, denoised22_low)
+    )
+    low_pass_mse_loss = low_pass_mse_loss_res + low_pass_mse_loss_cons
+
+    total_loss = mse_loss + low_pass_mse_loss + (1/10) * high_pass_mse_loss 
+    
+    return total_loss
